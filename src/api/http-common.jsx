f629c8e1ee90adaad80fdef68dd743edb236bc12
merwinfattah <18219019@std.stei.itb.ac.id>
2023-08-20 02:37:16 +0700

    change BaseUrl

--- src/api/http-common.jsx
+++ src/api/http-common.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://backend-midterm-production.up.railway.app/api",
     headers: {
         "Content-type": "application/json"
     }
--- src/api/http-get.jsx
+++ src/api/http-get.jsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const apiGet = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://backend-midterm-production.up.railway.app/api"
 });
